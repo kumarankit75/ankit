f3dfddb72b6df7c4b586dc1c07d2830e889d562a
Ankit kumar <125038894+kumarankit75@users.noreply.github.com>
2024-02-23 08:42:44 +0530

    Add files via upload

---
+++ img/index.js
@@ -0,0 +1,5 @@
+var count=14;
+var input = prompt("Enter some character");
+var inputcount = input.length();
+alert("you written" + inputcount +"character and you have left"+ count-inputcount +" character");
+
